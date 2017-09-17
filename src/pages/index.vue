72451376597a3828ba09317ee5c1a8017eae3f52
Fernando Moreira <nandomoreira.me@gmail.com>
2017-09-17 11:44:23 -0300

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+      <hello/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Hello from '~/components/Hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="sass">
+</style>
